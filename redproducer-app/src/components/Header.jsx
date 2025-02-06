362d54cd9dabd054c5dc982a02fe13d1a03e5e8c
=Samuel_Navasardyan <=sur00044@gmail.com>
2025-02-07 00:45:12 +0100

    Reorganización de CSS General, con reestructuración de tamaños y contenidos, y estilos mejorados

--- redproducer-app/src/components/Header.jsx
+++ redproducer-app/src/components/Header.jsx
@@ -1,14 +1,16 @@
 import "../css/Header.css"
+
 function Header(){
 	return(
 		<header className="header">
 			<a href="/">
-				<img src="/img/logo-app.png" alt="" className="icono-app"/>
+				<img src="/img/logo-app2.png" alt="" className="icono-app"/>
 			</a>
 			<nav className="nav">
 				<ul>
-					<li><a href="/">Inicio</a></li>
-					<li><a href="/panel">Panel</a></li>
+					<li><a href="/"><img src="/img/search_250dp_B3B3B3_FILL0_wght400_GRAD0_opsz48.png" alt="" /> Buscar</a></li>
+					<li><a href="/panel"><img src="/img/dock_to_right_250dp_B3B3B3_FILL0_wght400_GRAD0_opsz48.png" alt="" /> Panel</a></li>
+					<li><a href="/perfil"><img src="/img/account_circle_250dp_B3B3B3_FILL0_wght400_GRAD0_opsz48.png" alt="" /> Perfil</a></li>
 				</ul>
 			</nav>
 		</header>
--- redproducer-app/src/components/SeccionPrincipal.jsx
+++ redproducer-app/src/components/SeccionPrincipal.jsx
@@ -1,40 +1,36 @@
-import "../css/SeccionPrincipal.css"
-
+import "../css/SeccionPrincipal.css";
 
 import Cancion from "./Cancion";
-
 
 import canciones from "../data/canciones.json";
 
 function SeccionPrincipal() {
-	return (
-		<div className="contenedor-principal">
-			<div className="caja-buscador">
-				<input
-					type="text"
-					className="buscador"
-					placeholder="Buscar Canción..."
-				/>
-				<button className="boton-buscador">
-					<img
-						src="./../img/lupa.png" // Ruta desde la raíz del proyecto, sin necesidad de importar
-						alt="Buscar"
-					/>
-				</button>
-			</div>
-			<div className="contenedor-canciones">
-				{canciones.map((cancion, i) => (
-					<Cancion
-						key={i}
-						titulo={cancion.titulo}
-						portada={cancion.portada}
-						cancion={cancion} // Aquí pasas el objeto completo "cancion"
-						artista={cancion.artista}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  return (
+	<div className="contenedor-principal">
+	  <h1 className="titulo-pagina-inicio">Buscar Canciones</h1>
+	  <div className="caja-buscador">
+		<input
+		  type="text"
+		  className="buscador"
+		  placeholder="Buscar Canción..."
+		/>
+		<button className="boton-buscador">
+		  <img src="./../img/search_250dp_B3B3B3_FILL0_wght400_GRAD0_opsz48.png" alt="Buscar" />
+		</button>
+	  </div>
+	  <div className="contenedor-canciones">
+		{canciones.map((cancion, i) => (
+		  <Cancion
+			key={i}
+			titulo={cancion.titulo}
+			portada={cancion.portada}
+			cancion={cancion}
+			artista={cancion.artista}
+		  />
+		))}
+	  </div>
+	</div>
+  );
 }
 
 export default SeccionPrincipal;
