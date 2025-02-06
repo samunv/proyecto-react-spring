68c21ef991bafccf8ce32438801c98aaaffe5a44
Alejandro Yepes <alejandroyg3@gmail.com>
2025-02-06 11:44:19 +0100

    Agregada la interfaz visual del componente Reproductor sin funcionalidad

--- redproducer-app/src/components/Reproductor.jsx
+++ redproducer-app/src/components/Reproductor.jsx
@@ -1,1 +1,28 @@
-import "../css/Reproductor.css"
+import React from "react";
+import "../css/Reproductor.css";
+
+const Reproductor = () => {
+  return (
+    <div className="reproductor">
+      {/* Imagen de portada */}
+      <div className="cover">
+        <div className="music-icon">🎵</div>
+      </div>
+
+      {/* Información de la canción */}
+      <h3 className="title">Título</h3>
+      <p className="artist">Artista</p>
+
+      {/* Barra de progreso (sin funcionalidad aún) */}
+      <input type="range" min="0" max="100" value="30" readOnly />
+
+      {/* Tiempo actual */}
+      <p className="time">1:10</p>
+
+      {/* Botón de reproducción (aún sin funcionalidad) */}
+      <button className="play-button">▶️</button>
+    </div>
+  );
+};
+
+export default Reproductor;
