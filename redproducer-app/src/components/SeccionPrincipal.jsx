08a49ec0c22c669d5db92066b0f40a902891f0d5
=Samuel_Navasardyan <=sur00044@gmail.com>
2025-02-07 09:47:14 +0100

    css y buscador

--- redproducer-app/src/components/SeccionPrincipal.jsx
+++ redproducer-app/src/components/SeccionPrincipal.jsx
@@ -1,35 +1,50 @@
 import "../css/SeccionPrincipal.css";
-
+import { useState } from "react";
 import Cancion from "./Cancion";
-
 import canciones from "../data/canciones.json";
 
 function SeccionPrincipal() {
+  const [busqueda, setBusqueda] = useState("");
+
+  // Filtrar las canciones según la búsqueda
+  const filtrarElementos = canciones.filter((cancion) =>
+    cancion.titulo.toLowerCase().includes(busqueda.toLowerCase())
+  );
+
   return (
-	<div className="contenedor-principal">
-	  <h1 className="titulo-pagina-inicio">Buscar Canciones</h1>
-	  <div className="caja-buscador">
-		<input
-		  type="text"
-		  className="buscador"
-		  placeholder="Buscar Canción..."
-		/>
-		<button className="boton-buscador">
-		  <img src="./../img/search_250dp_B3B3B3_FILL0_wght400_GRAD0_opsz48.png" alt="Buscar" />
-		</button>
-	  </div>
-	  <div className="contenedor-canciones">
-		{canciones.map((cancion, i) => (
-		  <Cancion
-			key={i}
-			titulo={cancion.titulo}
-			portada={cancion.portada}
-			cancion={cancion}
-			artista={cancion.artista}
-		  />
-		))}
-	  </div>
-	</div>
+    <div className="contenedor-principal">
+      <h1 className="titulo-pagina-inicio">Buscar Canciones</h1>
+      <div className="caja-buscador">
+        <input
+          type="text"
+          className="buscador"
+          placeholder="Buscar Canción..."
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+        />
+        <button className="boton-buscador">
+          <img
+            src="./../img/search_250dp_B3B3B3_FILL0_wght400_GRAD0_opsz48.png"
+            alt="Buscar"
+          />
+        </button>
+      </div>
+      <div className="contenedor-canciones">
+        {filtrarElementos.length > 0 ? (
+          filtrarElementos.map((cancion, i) => (
+            <Cancion
+              key={i}
+              titulo={cancion.titulo}
+              portada={cancion.portada}
+              cancion={cancion}
+              artista={cancion.artista}
+            />
+          ))
+        ) : (
+          <p className="no-results-p">No se encontraron canciones.</p>
+        )}
+      </div>
+    </div>
   );
 }
 
