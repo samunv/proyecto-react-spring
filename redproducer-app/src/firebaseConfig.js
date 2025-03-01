a2f6c6a23cd2071ae47fd90e465b952243fa1ac1
Angel <angelgarcia05n@gmail.com>
2025-03-01 16:08:05 +0100

    Creamos conexión a Firebase

---
+++ redproducer-app/src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
