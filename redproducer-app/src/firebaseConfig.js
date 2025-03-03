1b4c988779486de139a13750171bd0d0d658bc9d
Jorge <jorgeanton1810@gmail.com>
2025-03-03 13:36:16 +0100

    Arreglos

--- redproducer-app/src/firebaseConfig.js
+++ redproducer-app/src/firebaseConfig.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBydOm4TxYyFD87oSCxN0mbjsxT-bhjMOQ",
   authDomain: "redproduce-app.firebaseapp.com",
   projectId: "redproduce-app",
-  storageBucket: "redproduce-app.appspot.com", // ✅ CORREGIDO
+  storageBucket: "redproduce-app.appspot.com", 
   messagingSenderId: "945822873251",
   appId: "1:945822873251:web:55081419b54178f223f116"
 };
